cf6936bc6799e722466f107e6b104d6607e458c2
JeshuaBen <dev.09@pbhub.com.br>
2023-03-12 11:04:03 -0300

    cssInJs #time 30m feat: created Header component and a variant that shows a back button if the screen haveit

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,10 @@
 import * as S from "./styles";
+import { Header } from "@components/Header";
 
 export const Groups: React.FC = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
 };
