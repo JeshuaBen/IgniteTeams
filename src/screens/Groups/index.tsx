a8802caf3a18be41d80bc6c9c7adf3f9ac033435
JeshuaBen <dev.09@pbhub.com.br>
2023-04-23 16:26:37 -0300

    cssInJs #time 1h feat: creating components and styling the application

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Highlight } from "@components/Highlight";
 import * as S from "./styles";
 import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
 
 export const Groups: React.FC = () => {
+  const [groups, setGroups] = useState<string[]>(["Galera do Ignite"]);
+
   return (
     <S.Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </S.Container>
   );
 };
