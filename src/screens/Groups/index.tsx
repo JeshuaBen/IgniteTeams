44917b7f7a9f27959bf5d7acbb4eaae9c2999acf
JeshuaBen <dev.09@pbhub.com.br>
2023-06-19 12:10:10 -0300

    feat: created ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from "@components/Highlight";
 import * as S from "./styles";
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
+import ListEmpty from "@components/ListEmpty";
 
 export const Groups: React.FC = () => {
-  const [groups, setGroups] = useState<string[]>(["Galera do Ignite"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <S.Container>
@@ -17,6 +18,10 @@ export const Groups: React.FC = () => {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </S.Container>
   );
